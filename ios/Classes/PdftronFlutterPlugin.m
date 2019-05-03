55cdfcde5b962f9df3a32fcc55413836fa54433d
James Borthwick <jborthwick@pdftron.com>
2019-05-02 17:30:20 -0700

    skeleton of configuring viewer from json object
    Signed-off-by: James Borthwick <jborthwick@pdftron.com>

--- ios/Classes/PdftronFlutterPlugin.m
+++ ios/Classes/PdftronFlutterPlugin.m
@@ -10,6 +10,37 @@
   [registrar addMethodCallDelegate:instance channel:channel];
 }
 
+-(void)configureDocumentViewController:(PTDocumentViewController*)documentViewController withConfig:(NSString*)config
+{
+    //convert from json to dict
+    NSData* jsonData = [config dataUsingEncoding:NSUTF8StringEncoding];
+    id foundationObject = [NSJSONSerialization JSONObjectWithData:jsonData options:kNilOptions error:Nil];
+
+    NSAssert( [foundationObject isKindOfClass:[NSDictionary class]], @"config JSON object not in expected dictionary format." );
+    
+    if( [foundationObject isKindOfClass:[NSDictionary class]] )
+    {
+        NSDictionary* configPairs = (NSDictionary*)foundationObject;
+        
+        for (NSString* key in configPairs.allKeys) {
+            if( [key isEqualToString:@"foo"] )
+            {
+                
+            }
+            else if( [key isEqualToString:@"bar"] )
+            {
+                
+            }
+        }
+    }
+    else
+    {
+        NSLog(@"config JSON object not in expected dictionary format.");
+    }
+    
+    
+}
+
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"getPlatformVersion" isEqualToString:call.method]) {
     result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
@@ -21,6 +52,8 @@
   } else if ([@"openDocument" isEqualToString:call.method]) {
       NSString *document = call.arguments[@"document"];
       
+     
+      
       if (document == nil || document.length == 0) {
           // error handling
           return;
@@ -29,10 +62,15 @@
       // Create and wrap a tabbed controller in a navigation controller.
       self.documentViewController = [[PTDocumentViewController alloc] init];
       
+      
       UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:self.documentViewController];
       
       self.documentViewController.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(topLeftButtonPressed:)];
       
+       NSString* config = call.arguments[@"config"];
+      
+      [self configureDocumentViewController:self.documentViewController withConfig:config];
+    
       // Open a file URL.
       NSURL *fileURL = [[NSBundle mainBundle] URLForResource:document withExtension:@"pdf"];
       if ([document containsString:@"://"]) {
