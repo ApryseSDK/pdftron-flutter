950a6a0a250dcdb588215e41f4191fc360c3760e
David Luco <30600417+dluco-pdftron@users.noreply.github.com>
2019-08-30 12:21:48 -0700

    Fix opening of documents w/o password.

--- ios/Classes/PdftronFlutterPlugin.m
+++ ios/Classes/PdftronFlutterPlugin.m
@@ -218,53 +218,67 @@ static NSString * const PTDisabledElementsKey = @"disabledElements";
     
 }
 
-- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else if ([@"getVersion" isEqualToString:call.method]) {
-      result([@"PDFNet " stringByAppendingFormat:@"%f", [PTPDFNet GetVersion]]);
-  } else if ([@"initialize" isEqualToString:call.method]) {
-      NSString *licenseKey = call.arguments[@"licenseKey"];
-      [PTPDFNet Initialize:licenseKey];
-  } else if ([@"openDocument" isEqualToString:call.method]) {
-      NSString *document = call.arguments[@"document"];
-      
-      NSString *password = call.arguments[@"password"];
-      
-      if (document == nil || document.length == 0) {
-          // error handling
-          return;
-      }
-      
-      // Create and wrap a tabbed controller in a navigation controller.
-      self.documentViewController = [[PTDocumentViewController alloc] init];
-      
-      
-      UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:self.documentViewController];
-      
-      self.documentViewController.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(topLeftButtonPressed:)];
-      
-       NSString* config = call.arguments[@"config"];
-      
-      [self configureDocumentViewController:self.documentViewController withConfig:config];
+- (void)handleOpenDocumentMethod:(NSDictionary<NSString *, id> *)arguments
+{
+    // Get document argument.
+    NSString *document = nil;
+    id documentValue = arguments[@"document"];
+    if ([documentValue isKindOfClass:[NSString class]]) {
+        document = (NSString *)documentValue;
+    }
     
-      // Open a file URL.
-      NSURL *fileURL = [[NSBundle mainBundle] URLForResource:document withExtension:@"pdf"];
-      if ([document containsString:@"://"]) {
-          fileURL = [NSURL URLWithString:document];
-      } else if ([document hasPrefix:@"/"]) {
-          fileURL = [NSURL fileURLWithPath:document];
-      }
-      
-      [self.documentViewController openDocumentWithURL:fileURL password:password];
-      
-      UIViewController *presentingViewController = [UIApplication sharedApplication].delegate.window.rootViewController;
-      
-      // Show navigation (and tabbed) controller.
-      [presentingViewController presentViewController:navigationController animated:YES completion:nil];
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
+    if (document.length == 0) {
+        // error handling
+        return;
+    }
+    
+    // Get (optional) password argument.
+    NSString *password = nil;
+    id passwordValue = arguments[@"password"];
+    if ([passwordValue isKindOfClass:[NSString class]]) {
+        password = (NSString *)passwordValue;
+    }
+    
+    // Create and wrap a tabbed controller in a navigation controller.
+    self.documentViewController = [[PTDocumentViewController alloc] init];
+    
+    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:self.documentViewController];
+    
+    self.documentViewController.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(topLeftButtonPressed:)];
+    
+    NSString* config = arguments[@"config"];
+    
+    [self configureDocumentViewController:self.documentViewController withConfig:config];
+    
+    // Open a file URL.
+    NSURL *fileURL = [[NSBundle mainBundle] URLForResource:document withExtension:@"pdf"];
+    if ([document containsString:@"://"]) {
+        fileURL = [NSURL URLWithString:document];
+    } else if ([document hasPrefix:@"/"]) {
+        fileURL = [NSURL fileURLWithPath:document];
+    }
+    
+    [self.documentViewController openDocumentWithURL:fileURL password:password];
+    
+    UIViewController *presentingViewController = UIApplication.sharedApplication.keyWindow.rootViewController;
+    
+    // Show navigation (and tabbed) controller.
+    [presentingViewController presentViewController:navigationController animated:YES completion:nil];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+    if ([@"getPlatformVersion" isEqualToString:call.method]) {
+        result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+    } else if ([@"getVersion" isEqualToString:call.method]) {
+        result([@"PDFNet " stringByAppendingFormat:@"%f", [PTPDFNet GetVersion]]);
+    } else if ([@"initialize" isEqualToString:call.method]) {
+        NSString *licenseKey = call.arguments[@"licenseKey"];
+        [PTPDFNet Initialize:licenseKey];
+    } else if ([@"openDocument" isEqualToString:call.method]) {
+        [self handleOpenDocumentMethod:call.arguments];
+    } else {
+        result(FlutterMethodNotImplemented);
+    }
 }
 
 - (void)topLeftButtonPressed:(UIBarButtonItem *)barButtonItem
