19e5c1147565ffe0c8770382e4bad43cde7cc731
sgong-pdftron <sgong@pdftron.com>
2020-05-29 13:25:27 -0700

    update

--- example/android/app/src/main/java/com/pdftron/pdftronflutterexample/FlutterAppCompatActivity.java
+++ example/android/app/src/main/java/com/pdftron/pdftronflutterexample/FlutterAppCompatActivity.java
@@ -5,120 +5,151 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+
 import io.flutter.app.FlutterActivityDelegate;
 import io.flutter.app.FlutterActivityDelegate.ViewFactory;
 import io.flutter.app.FlutterActivityEvents;
 import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.view.FlutterNativeView;
 import io.flutter.view.FlutterView;
-import io.flutter.view.FlutterView.Provider;
 
-public class FlutterAppCompatActivity extends AppCompatActivity implements Provider, PluginRegistry, ViewFactory {
+public class FlutterAppCompatActivity extends AppCompatActivity implements
+        FlutterView.Provider, PluginRegistry, ViewFactory {
     private final FlutterActivityDelegate delegate = new FlutterActivityDelegate(this, this);
-    private final FlutterActivityEvents eventDelegate;
-    private final Provider viewProvider;
-    private final PluginRegistry pluginRegistry;
 
-    public FlutterAppCompatActivity() {
-        this.eventDelegate = this.delegate;
-        this.viewProvider = this.delegate;
-        this.pluginRegistry = this.delegate;
-    }
+    // These aliases ensure that the methods we forward to the delegate adhere
+    // to relevant interfaces versus just existing in FlutterActivityDelegate.
+    private final FlutterActivityEvents eventDelegate = delegate;
+    private final FlutterView.Provider viewProvider = delegate;
+    private final PluginRegistry pluginRegistry = delegate;
 
+    /**
+     * Returns the Flutter view used by this activity; will be null before {@link #onCreate(Bundle)}
+     * is called.
+     */
+    @Override
     public FlutterView getFlutterView() {
-        return this.viewProvider.getFlutterView();
+        return viewProvider.getFlutterView();
     }
 
+    /**
+     * Hook for subclasses to customize the creation of the {@code FlutterView}.
+     *
+     * <p>The default implementation returns {@code null}, which will cause the activity to use a
+     * newly instantiated full-screen view.
+     */
+    @Override
     public FlutterView createFlutterView(Context context) {
         return null;
     }
 
+    @Override
     public FlutterNativeView createFlutterNativeView() {
         return null;
     }
 
+    @Override
     public boolean retainFlutterNativeView() {
         return false;
     }
 
+    @Override
     public final boolean hasPlugin(String key) {
-        return this.pluginRegistry.hasPlugin(key);
+        return pluginRegistry.hasPlugin(key);
     }
 
+    @Override
     public final <T> T valuePublishedByPlugin(String pluginKey) {
-        return this.pluginRegistry.valuePublishedByPlugin(pluginKey);
+        return pluginRegistry.valuePublishedByPlugin(pluginKey);
     }
 
+    @Override
     public final Registrar registrarFor(String pluginKey) {
-        return this.pluginRegistry.registrarFor(pluginKey);
+        return pluginRegistry.registrarFor(pluginKey);
     }
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.eventDelegate.onCreate(savedInstanceState);
+        eventDelegate.onCreate(savedInstanceState);
     }
 
+    @Override
     protected void onDestroy() {
-        this.eventDelegate.onDestroy();
+        eventDelegate.onDestroy();
         super.onDestroy();
     }
 
+    @Override
     public void onBackPressed() {
-        if (!this.eventDelegate.onBackPressed()) {
+        if (!eventDelegate.onBackPressed()) {
             super.onBackPressed();
         }
     }
 
+    @Override
     protected void onStart() {
         super.onStart();
-        this.eventDelegate.onStart();
+        eventDelegate.onStart();
     }
 
+    @Override
     protected void onStop() {
-        this.eventDelegate.onStop();
+        eventDelegate.onStop();
         super.onStop();
     }
 
+    @Override
     protected void onPause() {
         super.onPause();
-        this.eventDelegate.onPause();
+        eventDelegate.onPause();
     }
 
+    @Override
     protected void onPostResume() {
         super.onPostResume();
-        this.eventDelegate.onPostResume();
+        eventDelegate.onPostResume();
     }
 
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        this.eventDelegate.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        eventDelegate.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (!this.eventDelegate.onActivityResult(requestCode, resultCode, data)) {
+        if (!eventDelegate.onActivityResult(requestCode, resultCode, data)) {
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
 
+    @Override
     protected void onNewIntent(Intent intent) {
-        this.eventDelegate.onNewIntent(intent);
+        super.onNewIntent(intent);
+        eventDelegate.onNewIntent(intent);
     }
 
+    @Override
     public void onUserLeaveHint() {
-        this.eventDelegate.onUserLeaveHint();
+        eventDelegate.onUserLeaveHint();
     }
 
+    @Override
     public void onTrimMemory(int level) {
-        this.eventDelegate.onTrimMemory(level);
+        eventDelegate.onTrimMemory(level);
     }
 
+    @Override
     public void onLowMemory() {
-        this.eventDelegate.onLowMemory();
+        eventDelegate.onLowMemory();
     }
 
+    @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-        this.eventDelegate.onConfigurationChanged(newConfig);
+        eventDelegate.onConfigurationChanged(newConfig);
     }
 }
