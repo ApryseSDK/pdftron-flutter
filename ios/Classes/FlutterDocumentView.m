66e4835d7ee1ef8f4678ea0dccbca36c572e546c
Maxwell-Yang-PDFTron <myang@pdftron.com>
2020-09-11 15:41:25 -0700

    Implement iOS-side for the functions

--- ios/Classes/FlutterDocumentView.m
+++ ios/Classes/FlutterDocumentView.m
@@ -29,9 +29,14 @@
 
 @end
 
-@implementation FlutterDocumentView {
+@interface FlutterDocumentView() <PTTabbedDocumentViewControllerDelegate, PTDocumentViewControllerDelegate>
+
+@end
+
+@implementation FlutterDocumentView  {
     int64_t _viewId;
     FlutterMethodChannel* _channel;
+    FlutterResult flutterResult;
     PTDocumentViewController* _documentViewController;
     UINavigationController* _navigationController;
     UITextView* _textView;
@@ -90,13 +95,24 @@ static NSString * _Nullable PT_idAsNSString(id value)
             config = nil;
         }
         
-        [self openDocument:document password:password config:config];
+        [self openDocument:document password:password config:config resultToken:result];
+    } else if ([call.method isEqualToString:@"importAnnotationCommand"]) {
+        NSString* command = PT_idAsNSString(call.arguments[@"xfdfCommand"]);
+        
+        [self importAnnotationCommand:command];
+    } else if ([call.method isEqualToString:@"importBookmarkJson"]) {
+        NSString* bookmarkJson = PT_idAsNSString(call.arguments[@"bookmarkJson"]);
+        
+        [self importBookmarks:bookmarkJson];
+    } else if ([call.method isEqualToString:@"saveDocument"]) {
+        
+        [self saveDocument:result];
     } else {
         result(FlutterMethodNotImplemented);
     }
 }
 
-- (void)openDocument:(NSString *)document password:(NSString *)password config:(NSString *)config
+- (void)openDocument:(NSString *)document password:(NSString *)password config:(NSString *)config resultToken:(FlutterResult)result
 {
     if (!_documentViewController) {
         return;
@@ -113,7 +129,142 @@ static NSString * _Nullable PT_idAsNSString(id value)
         fileURL = [NSURL fileURLWithPath:document];
     }
     
+    
+    flutterResult = result;
+    [_documentViewController setDelegate:self];
     [_documentViewController openDocumentWithURL:fileURL password:password];
+}
+
+- (void)importAnnotationCommand:(NSString *)command
+{
+    
+    if (!_documentViewController) {
+        return;
+    }
+    
+    if( _documentViewController.document == Nil )
+    {
+        // something is wrong, no document.
+        NSLog(@"Error: The document view controller has no document.");
+        return;
+    }
+    
+    NSError* error;
+    
+    [_documentViewController.pdfViewCtrl DocLock:YES withBlock:^(PTPDFDoc * _Nullable doc) {
+        if( [doc HasDownloader] )
+        {
+            // too soon
+            NSLog(@"Error: The document is still being downloaded.");
+            return;
+        }
+
+        PTFDFDoc* fdfDoc = [doc FDFExtract:e_ptboth];
+        [fdfDoc MergeAnnots:command permitted_user:@""];
+        [doc FDFUpdate:fdfDoc];
+
+        [_documentViewController.pdfViewCtrl Update:YES];
+
+
+    } error:&error];
+}
+
+-(void)importBookmarks:(NSString *)bookmarkJson
+{
+    
+    if (!_documentViewController) {
+        return;
+    }
+    
+    if( _documentViewController.document == Nil )
+    {
+        // something is wrong, no document.
+        NSLog(@"Error: The document view controller has no document.");
+        return;
+    }
+    
+    NSError* error;
+    
+    [_documentViewController.pdfViewCtrl DocLock:YES withBlock:^(PTPDFDoc * _Nullable doc) {
+        if( [doc HasDownloader] )
+        {
+            // too soon
+            NSLog(@"Error: The document is still being downloaded.");
+            return;
+        }
+
+        [PTBookmarkManager.defaultManager importBookmarksForDoc:doc fromJSONString:bookmarkJson];
+
+
+    } error:&error];
+}
+
+-(void)saveDocument:(FlutterResult)result
+{
+    
+    if (!_documentViewController) {
+        return;
+    }
+    
+    __block NSString* resultString;
+    
+    if( _documentViewController.document == Nil )
+    {
+        resultString = @"Error: The document view controller has no document.";
+        
+        // something is wrong, no document.
+        NSLog(@"%@", resultString);
+        result(resultString);
+        
+        return;
+    }
+    
+    NSError* error;
+    
+    [_documentViewController.pdfViewCtrl DocLock:YES withBlock:^(PTPDFDoc * _Nullable doc) {
+        if( [doc HasDownloader] )
+        {
+            // too soon
+            resultString = @"Error: The document is still being downloaded and cannot be saved.";
+            NSLog(@"%@", resultString);
+            result(resultString);
+            return;
+        }
+
+        [_documentViewController saveDocument:0 completionHandler:^(BOOL success) {
+            if(!success)
+            {
+                resultString = @"Error: The file could not be saved.";
+                NSLog(@"%@", resultString);
+                result(resultString);
+            }
+            else
+            {
+                resultString = @"The file was successfully saved.";
+                result(resultString);
+            }
+        }];
+
+
+    } error:&error];
+    
+    if( error )
+    {
+        NSLog(@"Error: There was an error while trying to save the document. %@", error.localizedDescription);
+    }
+}
+
+- (void)documentViewControllerDidOpenDocument:(PTDocumentViewController *)documentViewController
+{
+    NSLog(@"Document opened successfully");
+    FlutterResult result = flutterResult;
+    result(@"Opened Document Successfully");
+}
+
+- (void)documentViewController:(PTDocumentViewController *)_documentViewController didFailToOpenDocumentWithError:(NSError *)error
+{
+    NSLog(@"Failed to open document: %@", error);
+    flutterResult([@"Opened Document Failed: %@" stringByAppendingString:error.description]);
 }
 
 @end
