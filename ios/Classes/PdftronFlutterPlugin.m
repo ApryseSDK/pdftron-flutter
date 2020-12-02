e36c412af481855cff664427e9241a6758706994
Maxwell-Yang-PDFTron <myang@pdftron.com>
2020-12-02 10:53:45 -0800

    Fix toolbar bug on null config iOS

--- ios/Classes/PdftronFlutterPlugin.m
+++ ios/Classes/PdftronFlutterPlugin.m
@@ -134,13 +134,15 @@
 
 + (void)configureDocumentViewController:(PTDocumentViewController*)documentViewController withConfig:(NSString*)config
 {
-    if (config.length == 0 || [config isEqualToString:@"null"]) {
-        return;
-    }
     
     PTFlutterViewController* flutterViewController = (PTFlutterViewController*)documentViewController;
     
     [flutterViewController initViewerSettings];
+    
+    if (config.length == 0 || [config isEqualToString:@"null"]) {
+        [flutterViewController applyViewerSettings];
+        return;
+    }
     
     //convert from json to dict
     id foundationObject = [PdftronFlutterPlugin PT_JSONStringToId:config];
