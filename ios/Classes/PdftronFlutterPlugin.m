4bca4d7f5d8ae2dc19d374c2124b826f4a2bc710
Maxwell-Yang-PDFTron <70409796+Maxwell-Yang-PDFTron@users.noreply.github.com>
2020-12-04 11:11:36 -0800

    iOS save-document promise (#54)
    * update iOS result handling
    
    * bump version
    
    * Remove unnecessary openDocument result handling
    
    * update save document promise value for iOS

--- ios/Classes/PdftronFlutterPlugin.m
+++ ios/Classes/PdftronFlutterPlugin.m
@@ -1152,7 +1152,6 @@
 - (void)saveDocument:(FlutterResult)flutterResult
 {
     PTDocumentViewController *docVC = [self getDocumentViewController];
-    __block NSString* resultString;
 
     if(docVC.document == Nil)
     {
@@ -1189,7 +1188,7 @@
         NSLog(@"Error: There was an error while trying to save document. %@", error.localizedDescription);
         flutterResult([FlutterError errorWithCode:@"save_document" message:@"Failed to save document" details:@"Error: There was an error while trying to save document."]);
     } else {
-        flutterResult(nil);
+        flutterResult(docVC.coordinatedDocument.fileURL.path);
     }
 }
 
