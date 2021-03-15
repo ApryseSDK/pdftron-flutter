4d267fc949f2909a09f68fa3ca1f6d966c8b18a2
Maxwell-Yang-PDFTron <myang@pdftron.com>
2021-03-15 11:29:20 -0700

    Include guide link in script file

--- JSONGeneration.sh
+++ JSONGeneration.sh
@@ -1,3 +1,5 @@
+# For detailed guide, please see: https://github.com/PDFTron/pdftron-flutter/wiki/JSON-generation-script
+
 # run this script to generate/update JSON Serialization boilerplate code for files in lib folder
 echo "Generating JSON Serialization for files in lib folder..."
 flutter packages pub run build_runner build
