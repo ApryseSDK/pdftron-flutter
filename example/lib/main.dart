d18cefb4537c75e4b1ac0f4bf80a5f031d0a9877
Saeed <82402654+sbazzaz@users.noreply.github.com>
2021-05-11 14:04:21 -0700

    produce problem case

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,194 +1,96 @@
 import 'dart:async';
-import 'dart:io' show Platform;
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:pdftron_flutter/pdftron_flutter.dart';
-import 'package:permission_handler/permission_handler.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MaterialApp(
+    title: 'Example',
+    home: FirstRoute(),
+  ));
+}
 
-class MyApp extends StatelessWidget {
+class FirstRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Viewer(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Route'),
+      ),
+      body: Center(
+          child: Column(
+              children: [
+                ElevatedButton(
+                  child: Text('Open PDFTronWidget'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Viewer(key: UniqueKey())),
+                    );
+                  },
+                ),
+              ]
+          )
+      ),
     );
   }
 }
 
 class Viewer extends StatefulWidget {
+
+  Viewer({Key key}) : super(key: key);
+
   @override
   _ViewerState createState() => _ViewerState();
 }
 
 class _ViewerState extends State<Viewer> {
   String _version = 'Unknown';
-  String _document =
-      "https://pdftron.s3.amazonaws.com/downloads/pl/PDFTRON_mobile_about.pdf";
+  String _document = "http://www.africau.edu/images/default/sample.pdf";
   bool _showViewer = true;
 
   @override
   void initState() {
     super.initState();
     initPlatformState();
-
-    if (Platform.isIOS) {
-      // Open the document for iOS, no need for permission
-      showViewer();
-    } else {
-      // Request for permissions for android before opening document
-      launchWithPermission();
-    }
   }
 
-  Future<void> launchWithPermission() async {
-    Map<PermissionGroup, PermissionStatus> permissions =
-        await PermissionHandler().requestPermissions([PermissionGroup.storage]);
-    if (granted(permissions[PermissionGroup.storage])) {
-      showViewer();
-    }
-  }
-
-  bool granted(PermissionStatus status) {
-    return status == PermissionStatus.granted;
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
     String version;
-    // Platform messages may fail, so we use a try/catch PlatformException.
     try {
       PdftronFlutter.initialize("your_pdftron_license_key");
       version = await PdftronFlutter.version;
     } on PlatformException {
       version = 'Failed to get platform version.';
     }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
     if (!mounted) return;
-
     setState(() {
       _version = version;
     });
   }
 
-  void showViewer() async {
-    // opening without a config file will have all functionality enabled.
-    // await PdftronFlutter.openDocument(_document);
-
-    // shows how to disale functionality
-//      var disabledElements = [Buttons.shareButton, Buttons.searchButton];
-//      var disabledTools = [Tools.annotationCreateLine, Tools.annotationCreateRectangle];
-    var config = Config();
-//      config.disabledElements = disabledElements;
-//      config.disabledTools = disabledTools;
-//      config.multiTabEnabled = true;
-//      config.customHeaders = {'headerName': 'headerValue'};
-
-    var documentLoadedCancel = startDocumentLoadedListener((filePath) {
-      print("document loaded: $filePath");
-    });
-
-    await PdftronFlutter.openDocument(_document, config: config);
-
-    try {
-      PdftronFlutter.importAnnotationCommand(
-          "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-              "    <xfdf xmlns=\"http://ns.adobe.com/xfdf/\" xml:space=\"preserve\">\n" +
-              "      <add>\n" +
-              "        <square style=\"solid\" width=\"5\" color=\"#E44234\" opacity=\"1\" creationdate=\"D:20200619203211Z\" flags=\"print\" date=\"D:20200619203211Z\" name=\"c684da06-12d2-4ccd-9361-0a1bf2e089e3\" page=\"1\" rect=\"113.312,277.056,235.43,350.173\" title=\"\" />\n" +
-              "      </add>\n" +
-              "      <modify />\n" +
-              "      <delete />\n" +
-              "      <pdf-info import-version=\"3\" version=\"2\" xmlns=\"http://www.pdftron.com/pdfinfo\" />\n" +
-              "    </xfdf>");
-    } on PlatformException catch (e) {
-      print("Failed to importAnnotationCommand '${e.message}'.");
-    }
-
-    try {
-      PdftronFlutter.importBookmarkJson('{"0":"Page 1"}');
-    } on PlatformException catch (e) {
-      print("Failed to importBookmarkJson '${e.message}'.");
-    }
-
-    var annotCancel = startExportAnnotationCommandListener((xfdfCommand) {
-      // local annotation changed
-      // upload XFDF command to server here
-      print("flutter xfdfCommand: $xfdfCommand");
-    });
-
-    var bookmarkCancel = startExportBookmarkListener((bookmarkJson) {
-      print("flutter bookmark: $bookmarkJson");
-    });
-
-    var path = await PdftronFlutter.saveDocument();
-    print("flutter save: $path");
-
-    // to cancel event:
-    // annotCancel();
-    // bookmarkCancel();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('PdfTron'),
+      ),
       body: Container(
         width: double.infinity,
         height: double.infinity,
-        child:
-            // Uncomment this to use Widget version of the viewer
-            // _showViewer
-            // ? DocumentView(
-            //     onCreated: _onDocumentViewCreated,
-            //   ):
-            Container(),
+        child: _showViewer ? DocumentView(
+          onCreated: _onDocumentViewCreated,
+        ): Container(),
       ),
     );
   }
 
   void _onDocumentViewCreated(DocumentViewController controller) async {
-    Config config = new Config();
-
-    config.disabledElements = [Buttons.arrowToolButton];
-
     var leadingNavCancel = startLeadingNavButtonPressedListener(() {
-      // Uncomment this to quit the viewer when leading navigation button is pressed
-      // this.setState(() {
-      //   _showViewer = !_showViewer;
-      // });
-
-      // Show a dialog when leading navigation button is pressed
-      _showMyDialog();
+      Navigator.pop(context);
     });
-
-    controller.openDocument(_document, config: config);
+    controller.openDocument(_document);
   }
 
-  Future<void> _showMyDialog() async {
-    print('hello');
-    return showDialog<void>(
-      context: context,
-      barrierDismissible: false, // user must tap button!
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('AlertDialog'),
-          content: SingleChildScrollView(
-            child: Text('Leading navigation button has been pressed.'),
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: Text('OK'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
 }
