12c73dc8fe2463aaabfa9a8a52102927c52360e4
Dominic Cupidon <dcupidon@pdftron.com>
2021-06-03 14:59:05 -0700

    First steps to adding null safety to the SDK

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -43,15 +43,10 @@ class _ViewerState extends State<Viewer> {
   }
 
   Future<void> launchWithPermission() async {
-    Map<PermissionGroup, PermissionStatus> permissions =
-        await PermissionHandler().requestPermissions([PermissionGroup.storage]);
-    if (granted(permissions[PermissionGroup.storage])) {
+    PermissionStatus permission = await Permission.storage.request();
+    if (permission.isGranted) {
       showViewer();
     }
-  }
-
-  bool granted(PermissionStatus status) {
-    return status == PermissionStatus.granted;
   }
 
   // Platform messages are asynchronous, so we initialize in an async method.
