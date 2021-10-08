cd45b294a439dcb47072fd147e36f7f62deeecd0
akwanpdf <akwan@pdftron.com>
2021-10-08 16:53:50 -0700

    (Android) Remove flag since we are now calling from inside Activity context

--- android/src/main/java/com/pdftron/pdftronflutter/FlutterDocumentActivity.java
+++ android/src/main/java/com/pdftron/pdftronflutter/FlutterDocumentActivity.java
@@ -142,7 +142,7 @@ public class FlutterDocumentActivity extends DocumentActivity implements ViewerC
             intentBuilder.usingNavIcon(navIconId);
             intentBuilder.usingConfig(config);
             intentBuilder.usingNewUi(true);
-            packageContext.startActivity(intentBuilder.build().addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+            packageContext.startActivity(intentBuilder.build());
         }
     }
 
