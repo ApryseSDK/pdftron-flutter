2fee1575d4145524e48c8b6e07475f0755fe4e75
kmathew-pdftron <kmathew@pdftron.com>
2022-04-14 17:04:55 -0700

    update script

--- scripts/version_bump.bash
+++ scripts/version_bump.bash
@@ -1,7 +1,9 @@
-VERSION=$('yq .version pubspec.yaml')
+VERSION=$(yq '.version' pubspec.yaml)
 IFS='.' 
 read -a TOKENS <<< "$VERSION"
 v="$VERSION"
 UPDATE=$(expr ${TOKENS[3]} + 1) 
 IFS=''
-NEW_VERSION="${TOKENS[0]}.${TOKENS[1]}.${TOKENS[2]}.$UPDATE" yq -i '.version = strenv(NEW_VERSION)' pubspec.yaml
+NEW_VERSION="${TOKENS[0]}.${TOKENS[1]}.${TOKENS[2]}.$UPDATE" 
+echo $NEW_VERSION
+# yq -i '.version = strenv(NEW_VERSION)' pubspec.yaml
