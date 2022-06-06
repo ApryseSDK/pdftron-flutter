63810faa4aa7cac345c1586b49597b9e6da89ab6
Terry Yoon <tyoon@pdftron.com>
2022-06-06 11:45:35 -0700

    remove unused import

--- android/src/main/java/com/pdftron/pdftronflutter/helpers/PluginUtils.java
+++ android/src/main/java/com/pdftron/pdftronflutter/helpers/PluginUtils.java
@@ -70,7 +70,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
