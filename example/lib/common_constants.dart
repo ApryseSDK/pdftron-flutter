2f732a7dde416a77f10e9d33f8d8f76a5a4cb743
Arshad Nabi Mac <arshad.nabi@meetingdecisions.com>
2022-07-28 17:29:14 +0530

    implemented common constants

---
+++ example/lib/common_constants.dart
@@ -0,0 +1,8 @@
+class CommonConstants {
+  static const String annotationMenuItem_PDFTronKey = "annotationMenuItem";
+  static const String annotations_PDFTronKey = "annotations";
+  static const String DecisionsButtonClicked_PDFTronButton =
+      "DecisionsButtonClicked";
+
+  static const String ShareDecisions_PDFTronMenu = "ShareDecisions";
+}
