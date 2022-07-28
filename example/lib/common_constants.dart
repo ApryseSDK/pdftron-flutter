f92f58872003e9bf4b1d41075b5f406502e64556
Arshad Nabi Mac <arshad.nabi@meetingdecisions.com>
2022-07-28 17:31:49 +0530

    updated common constants

--- example/lib/common_constants.dart
+++ example/lib/common_constants.dart
@@ -1,8 +1,10 @@
 class CommonConstants {
+  //keys
   static const String annotationMenuItem_PDFTronKey = "annotationMenuItem";
   static const String annotations_PDFTronKey = "annotations";
+  //Button
   static const String DecisionsButtonClicked_PDFTronButton =
       "DecisionsButtonClicked";
-
+  //Menu
   static const String ShareDecisions_PDFTronMenu = "ShareDecisions";
 }
