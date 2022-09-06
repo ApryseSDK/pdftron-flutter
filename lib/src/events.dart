7c6bac0f4134333290073603ac8541b18583e838
Darren Chan <chanthedarren@gmail.com>
2022-09-06 09:33:21 -0700

    Update events.dart
    removing comment saying "Android Only"

--- lib/src/events.dart
+++ lib/src/events.dart
@@ -349,7 +349,7 @@ CancelListener startPageMovedListener(PageMovedListener listener) {
 ///
 /// Returns a function that can cancel the listener.
 /// Custom toolbar items can be added using the [Config.annotationToolbars]
-/// config. Android only.
+/// config. 
 CancelListener startAnnotationToolbarItemPressedListener(
     AnnotationToolbarItemPressedListener listener) {
   var subscription = _annotationToolbarItemPressedChannel
