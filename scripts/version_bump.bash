940450ecfb648ef6efecc6b58a07d46cbb16b907
jwong-pdftron <jwong@pdftron.com>
2022-11-23 15:26:33 -0800

    Remove whitespace

--- scripts/version_bump.bash
+++ scripts/version_bump.bash
@@ -2,6 +2,6 @@ VERSION=$(yq '.version' pubspec.yaml)
 IFS='-' 
 read -a TOKENS <<< "$VERSION"
 v="$VERSION"
-UPDATE=$(expr ${TOKENS[1]} + 1) 
+UPDATE=$(expr ${TOKENS[1]} + 1)
 IFS=''
 NEW_VERSION="${TOKENS[0]}-$UPDATE" yq -i '.version = strenv(NEW_VERSION)' pubspec.yaml
